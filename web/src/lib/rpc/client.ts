dd6854f6863af670d0a1d230f80887f20d536cb0
Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>
2026-06-29 08:00:22 +0900

    HolidaysServiceのimportに.js拡張子を復元
    NodeNext相当のモジュール解決でランタイムから解決できるよう、
    生成コードへの相対importに.js拡張子を付与する形へ戻した。

--- web/src/lib/rpc/client.ts
+++ web/src/lib/rpc/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@connectrpc/connect';
 import { createConnectTransport } from '@connectrpc/connect-web';
 import { env } from '$env/dynamic/public';
-import { HolidaysService } from '../gen/kantacky_calendar/v1/holidays_pb';
+import { HolidaysService } from '../gen/kantacky_calendar/v1/holidays_pb.js';
 
 const baseUrl = env.PUBLIC_API_BASE_URL ?? 'http://localhost:8787';
 
