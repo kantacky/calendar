8534ce239bb852cb78c5b3188b0c8511c0b35f39
Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>
2026-06-29 08:30:23 +0900

    Add ios

---
+++ ios/Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by 及川 寛太 on 2026/06/29.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendar
+//
+//  Created by 及川 寛太 on 2026/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
